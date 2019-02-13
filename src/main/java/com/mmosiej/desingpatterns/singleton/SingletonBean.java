a6fdc50d88bda23ef5c540c405bd78bb85d1216d
Marcin Mosiej <mmosiej@isolution.pl>
2019-02-13 21:58:41 +0100

    Remove unused imports.

--- src/main/java/com/mmosiej/desingpatterns/singleton/SingletonBean.java
+++ src/main/java/com/mmosiej/desingpatterns/singleton/SingletonBean.java
@@ -1,7 +1,5 @@
 package com.mmosiej.desingpatterns.singleton;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
